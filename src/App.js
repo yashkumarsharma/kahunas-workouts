f1c041e04b0c1190c40b36170fdca65499b01b8f
Yash Kumar Sharma <yks.nit@gmail.com>
2024-07-12 05:53:20 +0400

    fix: add missing StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
-import { Provider } from 'react-redux'
-import { useStore } from 'container/StoreContext'
+import { useSelector } from 'react-redux'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { StoreProvider } from 'container/StoreContext'
 
 import NutritionPage from './containers/NutritionPage'
 import LandingPage from './containers/LandingPage'
@@ -9,34 +9,15 @@ import i18n from './translations/i18n'
 
 import './translations/i18n'
 
-// Todo: Leaving some commented tested code for redux integration.
-// const rootHelloReducer = (state = {
-//   isLoading: true,
-// }, action) => state
-
 const App = () => {
-  // const { store, replaceReducertest } = useStore()
-  const { store } = useStore()
-
-  // const language = useSelector(state => state.appSettings.language)
-
-  // Todo: Move to a separate component under <Provider>, as it is very inefficient
-  const language = store.getState()?.appSettings?.language || 'en'
+  const language = useSelector(state => state.appSettings?.language || 'en')
 
   useEffect(() => {
     i18n.changeLanguage(language)
   }, [language])
 
-  // useEffect(() => {
-  //   if(!replaceReducertest) return
-  //   replaceReducertest({
-  //     rootHelloReducer: rootHelloReducer,
-  //     // Add other reducers here if needed
-  //   })
-  // }, [replaceReducertest])
-
   return (
-    <Provider store={store}>
+    <StoreProvider>
       <Router basename='/workouts'>
         <Routes>
           <Route path='/' element={<LandingPage />} />
@@ -44,7 +25,8 @@ const App = () => {
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </Router>
-    </Provider>
+    </StoreProvider>
+
   )
 }
 
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import store from './store/store'
 import App from './App'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
+// Todo: Configure Local store
 root.render(
   <React.StrictMode>
-    <App basePath='/workouts/' />
+    <Provider store={store}>
+      <App basePath='/workouts/' />
+    </Provider>
   </React.StrictMode>
 )
